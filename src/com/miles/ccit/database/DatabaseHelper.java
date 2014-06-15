a6e8ec929c3dc5ca34e385624871b7a9423a0736
Jasonniu1573 <Jason_1573@163.com>
2014-06-15 21:09:56 +0800

    测试添加数据库helper文件

---
+++ src/com/miles/ccit/database/DatabaseHelper.java
@@ -0,0 +1,124 @@
+package com.miles.ccit.database;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteDatabase.CursorFactory;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+//DatabaseHelper作为一个访问SQLite的助手类，提供两个方面的功能，
+//第一，getReadableDatabase(),getWritableDatabase()可以获得SQLiteDatabse对象，通过该对象可以对数据库进行操作
+//第二，提供了onCreate()和onUpgrade()两个回调函数，允许我们在创建和升级数据库时，进行自己的操作
+
+public class DatabaseHelper extends SQLiteOpenHelper
+{
+
+	private static final int VERSION = 1; 	// 数据库版本/2013/10/22
+	private static final String DATABASE_NAME = "DuoMo.db"; // 数据库名称
+
+	// 在SQLiteOepnHelper的子类当中，必须有该构造函数
+	public DatabaseHelper(Context context, String name, CursorFactory factory, int version)
+	{
+		// 必须通过super调用父类当中的构造函数
+		super(context, name, factory, version);
+		// TODO Auto-generated constructor stub
+	}
+
+	public DatabaseHelper(Context context)
+	{
+		this(context, DATABASE_NAME, VERSION);
+	}
+
+	public DatabaseHelper(Context context, String name, int version)
+	{
+		this(context, DATABASE_NAME, null, version);
+	}
+
+	// 该函数是在第一次创建数据库的时候执行,实际上是在第一次得到SQLiteDatabse对象的时候，才会调用这个方法
+	@Override
+	public void onCreate(SQLiteDatabase db)
+	{
+		// TODO Auto-generated method stub
+		CtreteTables(db);
+		initData(db);
+		Log.v("Jason", "Create Database");
+//		CtreteTables(db);
+//		initData(db);
+	}
+
+	private void CtreteTables(SQLiteDatabase db)
+	{
+		// execSQL函数用于执行SQL语句,创建表
+		db.execSQL("create table floors(floorid INTEGER PRIMARY KEY AUTOINCREMENT,myfloorid varchar(10) NOT NULL,floorname varchar(20) NOT NULL,floorinfo varchar(10),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		db.execSQL("create table rooms(roomid INTEGER PRIMARY KEY AUTOINCREMENT,myroomid varchar(10) NOT NULL,roomname varchar(20) NOT NULL,roomaddr varchar(20) NOT NULL,floorid INTEGER NOT NULL,roominfo varchar(10),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		db.execSQL("create table equips(equipid INTEGER PRIMARY KEY AUTOINCREMENT,myequipid varchar(10) NOT NULL,equipname varchar(20) NOT NULL,equipaddr varchar(20) NOT NULL,equipstyle varchar(20) NOT NULL,equipstatus varchar(20) NOT NULL,roomid varchar(10) NOT NULL,equipinfo varchar(10),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		db.execSQL("create table scenes(sceneid INTEGER PRIMARY KEY AUTOINCREMENT,scenename varchar(20) NOT NULL,sceneinfo varchar(20),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		db.execSQL("create table voices(voiceid INTEGER PRIMARY KEY AUTOINCREMENT,voicename varchar(20) NOT NULL,sceneid INTEGER NOT NULL,voiceinfo varchar(20),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		
+		//新增表-场景设备表
+		db.execSQL("create table scene_equips(relationid INTEGER PRIMARY KEY AUTOINCREMENT,sceneid INTEGER NOT NULL,equipid INTEGER NOT NULL,scenename varchar(20) NOT NULL,equipname varchar(20) NOT NULL,advancevalue varchar(20) NOT NULL,info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		db.execSQL("create table cameras(camera INTEGER PRIMARY KEY AUTOINCREMENT,cameraname varchar(20) NOT NULL,cameraip varchar(20) NOT NULL,cameraport varchar(20) NOT NULL,camerauser varchar(20) NOT NULL,camerapwd varchar(20) NOT NULL,info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+		
+	}
+
+	// 初始化密码表中数据，
+	private void initData(SQLiteDatabase db)
+	{
+		String Id = null;
+		ContentValues values = new ContentValues();
+		values.put("floorid", Id);
+		values.put("myfloorname", "0");
+		values.put("floorname", "一楼");
+		db.insert("floors", null, values);
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion)
+	{
+		// TODO Auto-generated method stub
+		if(newVersion > oldVersion)
+		{
+			db.execSQL("drop table scenes;");
+			db.execSQL("drop table voices;");
+			
+			db.execSQL("create table scenes(sceneid INTEGER PRIMARY KEY AUTOINCREMENT,scenename varchar(20) NOT NULL,sceneinfo varchar(20),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+			db.execSQL("create table voices(voiceid INTEGER PRIMARY KEY AUTOINCREMENT,voicename varchar(20) NOT NULL,sceneid INTEGER NOT NULL,voiceinfo varchar(20),info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+			
+			
+			
+			//删除旧表多余字段
+//			db.execSQL("alter table scenes drop column mysceneid;");
+//			db.execSQL("alter table scenes drop column equipstatus;");
+//			db.execSQL("alter table scenes drop column sceneaddr;");
+//			db.execSQL("alter table voices drop column myvoiceid;");
+			
+			//增加新表
+			db.execSQL("create table scene_equips(relationid INTEGER PRIMARY KEY AUTOINCREMENT,sceneid INTEGER NOT NULL,equipid INTEGER NOT NULL,scenename varchar(20) NOT NULL,equipname varchar(20) NOT NULL,advancevalue varchar(20) NOT NULL,info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+			db.execSQL("create table cameras(camera INTEGER PRIMARY KEY AUTOINCREMENT,cameraname varchar(20) NOT NULL,cameraip varchar(20) NOT NULL,cameraport varchar(20) NOT NULL,camerauser varchar(20) NOT NULL,camerapwd varchar(20) NOT NULL,info1 varchar(10),info2 varchar(10),info3 varchar(10))");
+			
+			String Id = null;
+			ContentValues values = new ContentValues();
+			values.put("voiceid", Id);
+			values.put("voicename", "回家");
+			values.put("sceneid", "-1");
+			db.insert("voices", null, values);
+			
+			values.put("voiceid", Id);
+			values.put("voicename", "会客");
+			values.put("sceneid", "-2");
+			db.insert("voices", null, values);
+			
+			values.put("voiceid", Id);
+			values.put("voicename", "外出");
+			values.put("sceneid", "-3");
+			db.insert("voices", null, values);
+			
+			values.put("voiceid", Id);
+			values.put("voicename", "娱乐");
+			values.put("sceneid", "-4");
+			db.insert("voices", null, values);
+			
+		}
+	}
+}
